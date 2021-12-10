7ecb5e11270845eef88a3090eec32909c18f1132
Guido Tello <lpmaleinn@gmail.com>
2021-12-10 04:47:50 -0300

    Added Artist to Credits

--- src/Scenes/Credits/credits.cpp
+++ src/Scenes/Credits/credits.cpp
@@ -8,8 +8,8 @@ namespace game
 
 	void initCreditsScreen()
 	{
-		button.height = 20;
-		button.width = static_cast<float>(MeasureText("Back", 15) + 5);
+		button.height = 30;
+		button.width = static_cast<float>(MeasureText("Back", 25) + 5);
 		button.x = 0;
 		button.y = 0;
 	}
@@ -30,15 +30,18 @@ namespace game
 
 		DrawRectangleRec(button, BLUE);
 		DrawRectangleLinesEx(button, 1, YELLOW);
-		DrawText("Back", static_cast<int>(button.x + button.width / 2 - MeasureText("Back", 15) / 2), static_cast<int>(button.y + 4), 15, BLACK);
+		DrawText("Back", static_cast<int>(button.x + button.width / 2 - MeasureText("Back", fontSize) / 2), static_cast<int>(button.y + 4), fontSize, BLACK);
 
 		DrawText("Credits", GetScreenWidth() / 2 - 100, 50, 60, YELLOW);
 
-		DrawText("Created by:", 0, 150, 35, YELLOW);
-		DrawText("GUZMAN, Ramiro (ORIGINAL)", 100, 225, fontSize, BLUE);
-		DrawText("TELLO, Guido (FORKED)", 100, 270, fontSize, BLUE);
+		DrawText("Developed by:", 0, 150, 35, YELLOW);
+		DrawText("GUZMAN, Ramiro (ORIGINAL)", 100, 200, fontSize, BLUE);
+		DrawText("TELLO, Guido (FORKED)", 100, 230, fontSize, BLUE);
 
+		DrawText("Art by:", 0, 270, 35, YELLOW);
+		DrawText("LARRIVEY, Agostina (Tubes)", 100, 320, fontSize, BLUE);
+		DrawText("Unknown (Parallax & Bird)", 100, 350, fontSize, BLUE);
 
-		DrawText("Version 0.3", GetScreenWidth() - MeasureText("Version 0.3", fontSize), GetScreenHeight() - 40, fontSize, RED);
+		DrawText("Version 1.0", GetScreenWidth() - MeasureText("Version 1.0", fontSize), GetScreenHeight() - 40, fontSize, RED);
 	}
 }
